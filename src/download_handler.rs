0d1d87432ac98f26180fb27e3e97d582a91780dc
Johannes Thoren <johannes.thoren@outlook.com>
2020-12-08 15:23:42 +0100

    poof!

--- src/download_handler.rs
+++ src/download_handler.rs
@@ -1,17 +1,18 @@
 use crate::post_handler;
 
 pub fn get_images(count: usize, posts: Vec<post_handler::Post>) {
-    let filetypes = ["png", "gif", "jpg"];
-
+    let filetypes = vec!["png", "jpg", "gif"];
     // if link is not ending with (jpg, gif or png)
     // then check next
     let mut i = 0;
     while i < count {
+        println!("{}:x", i);
+
         for filetype in &filetypes {
             if posts[i].post_url.as_str().ends_with(filetype) {
                 println!("{}", posts[i].post_url);
                 println!("{}",i);
-                i = i + 1;
+                i += 1;
             }
         }
     }
